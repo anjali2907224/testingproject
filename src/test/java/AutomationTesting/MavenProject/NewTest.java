173c68eb2d0b01297a2cdad722e4a875f5feff96
Amit Bhardwaj <amitbhardwaj6159@gmail.com>
2019-07-29 11:24:34 +0530

    chanages2  git commit -m changes .. ./
    cd..
    git cd
    git cd../
    git 
    

---
+++ src/test/java/AutomationTesting/MavenProject/NewTest.java
@@ -0,0 +1,17 @@
+package AutomationTesting.MavenProject;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  
+	  System.out.println(" learning automation");
+	  System.out.println("are you learning automation");
+	  
+	  
+	  System.out.println("Hello are you learning automation");
+	  
+	  
+  }
+}
